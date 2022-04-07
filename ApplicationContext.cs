9bcf9c99928d27d3eaaa48571c08b77812f8bb29
opanforever1992 <opanforever1992@gmail.com>
2022-04-07 15:20:21 +0300

    Add_newProj

---
+++ ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace PostegreSQLConnectTest
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql
+                (
+                    "Host = localhost;" +
+                    "Port = 5432;" +
+                    "Database = userdb;" +
+                    "User ID = postgres;" +
+                    "Password = 159357yarik101"
+                );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace PostegreSQLConnectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user1 = new() { Name = "Tom", Age = 33 };
+                User user2 = new() { Name = "Alice", Age = 25 };
+
+                db.Users.AddRange(user1, user2);
+                db.SaveChanges();
+            }
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.ToList();
+                System.Console.WriteLine("Users list:");
+                foreach (User u in users)
+                    System.Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+            }
+        }
+    }
+}
